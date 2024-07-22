5847cd5b9a8315509aa6842124bb0f2f8c3a4d57
Priyanshu Dharmshaktu <dharmshaktu23@gmail.com>
2024-07-22 23:31:05 +0530

    r

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import GetApp from "@/components/GetApp";
+import Features from "@/components/Features";
 
 export default function Home() {
-  return <h1 className="font-bold text-teal-500 text-2xl">Hello World</h1>;
+  return (
+    <>
+      <Hero></Hero>
+      <Camp></Camp>
+      <Guide></Guide>
+      <Features></Features>
+      <GetApp></GetApp>
+    </>
+  );
 }
